e05bfcda3157f1252f04df6b56b2f03a7f6bf86d
Nicolas Romano <nicolasromano2@gmail.com>
2022-11-16 12:09:12 -0300

    first commit

---
+++ backend/delipizza/src/main/java/com/mendoza/delipizza/DelipizzaApplication.java
@@ -0,0 +1,13 @@
+package com.mendoza.delipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelipizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelipizzaApplication.class, args);
+	}
+
+}
---
+++ backend/delipizza/src/test/java/com/mendoza/delipizza/DelipizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendoza.delipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelipizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
